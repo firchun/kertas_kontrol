97e65b8f107a8de41ef4d032965c1d9a6baf6c8e
firchun025 <firchun025@gmail.com>
2024-04-18 17:08:42 +0900

    fix bug chat

--- resources/views/pages/bimbingan/chat/chat.blade.php
+++ resources/views/pages/bimbingan/chat/chat.blade.php
@@ -65,13 +65,19 @@
             </div>
             <div class="bg-primary">
                 <form id="form" class="d-flex navbar-expand-sm d-flex justify-content-between py-3 mx-2 flex-bottom">
+
                     <input type="text" name="text" class="form-control" placeholder="Tulis pesan disini...">
+                    <button id="refreshButton" class="btn btn-light py-2 mx-2">
+                        <i class="fas fa-sync-alt"></i>
+                    </button>
+                    <div id="loadingIcon" class="px-5"
+                        style="display: none; font-size: 24px; color: white; width:50px;">
+                        <i class="fas fa-spinner fa-spin"></i>
+                    </div>
                     <button id="submitButton" class="btn btn-light py-2 mx-2">
                         <i class="fa fa-paper-plane"></i>
                     </button>
-                    <div id="loadingIcon" class="px-5" style="display: none; font-size: 24px; color: white;">
-                        <i class="fas fa-spinner fa-spin"></i>
-                    </div>
+
                 </form>
             </div>
         </div>
@@ -97,6 +103,18 @@
         });
     </script>
     <script>
+        document.getElementById('refreshButton').addEventListener('click', async () => {
+            const loadingIcon = document.getElementById('loadingIcon');
+            loadingIcon.style.display = 'block';
+
+            try {
+                await getChat('');
+                loadingIcon.style.display = 'none';
+            } catch (error) {
+                console.error('Error refreshing chat:', error);
+                loadingIcon.style.display = 'none';
+            }
+        });
         const getChat = async (lastMessageTime) => {
             try {
                 const response = await fetch(`/chat/get/{{ $room->id }}?last_message_time=${lastMessageTime}`);
@@ -115,21 +133,21 @@
                     });
 
                     chatsHTML += `
-                 <div class="d-flex align-items-center 
-                 ${r.id_user == "{{ Auth::user()->id }}" ? 'text-right justify-content-end' : ''}">
-                     <div class="pr-2 ${r.id_user == "{{ Auth::user()->id }}" ? '' : 'pl-1'}"> 
-                         <span class="name">${r.id_user == "{{ Auth::user()->id }}" ? 'Anda' : "{{ Auth::user()->role == 'mahasiswa' ? 'Dosen' : 'Mahasiswa' }}" } | ${timeString}</span>
-                         <p class="msg mb-0">${r.message}</p>
-                         <span class="read ${r.is_read == 0 ? 'text-muted' :'text-success' } ">${r.is_read == 0 ? 'Belum di baca' :'<i class="fa fa-check"></i> dibaca' } </span>
-                     </div>
-                 </div>`;
+                    <div class="d-flex align-items-center 
+                    ${r.id_user == "{{ Auth::user()->id }}" ? 'text-right justify-content-end' : ''}">
+                        <div class="pr-2 ${r.id_user == "{{ Auth::user()->id }}" ? '' : 'pl-1'}"> 
+                            <span class="name">${r.id_user == "{{ Auth::user()->id }}" ? 'Anda' : "{{ Auth::user()->role == 'mahasiswa' ? 'Dosen' : 'Mahasiswa' }}" } | ${timeString}</span>
+                            <p class="msg mb-0">${r.message}</p>
+                            <span class="read ${r.is_read == 0 ? 'text-muted' :'text-success' } ">${r.is_read == 0 ? 'Belum di baca' :'<i class="fa fa-check"></i> dibaca' } </span>
+                        </div>
+                    </div>`;
                 });
 
                 document.getElementById('message').innerHTML = chatsHTML;
             } catch (error) {
                 console.error('Error fetching chat:', error);
             }
-        }
+        };
 
         window.addEventListener('load', async () => {
             Pusher.logToConsole = true;
@@ -137,7 +155,6 @@
                 cluster: "{{ env('PUSHER_APP_CLUSTER') }}"
             });
 
-            // const channel = pusher.subscribe('chat-channel');
             const channel = pusher.subscribe('client-chat-channel');
 
             channel.bind('chat-send', async (data) => {
